26c93eff77f4f20f96739d7d98ca552517433939
disha2292 <disha2292@icloud.com>
2024-11-13 08:26:48 +0530

    added arrays.cpp

---
+++ Array/array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//   int arr[5];
+//   cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+
+//   arr[3]+=10;
+//   cout<<arr[3];
+
+//   return 0;
+
+//     return 0;
+// }
+
+int main(){
+    int arr[3][5];
+
+    arr[1][3]=78;
+    cout<<arr[1][3];
+  
+
+    return 0;
+}
